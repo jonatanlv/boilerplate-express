837823d6bd49b312b9354ac015b6402fb102dbb6
jonatan.lara <jonatan.lara@gft.com>
2021-08-19 16:56:18 +0200

    Part 6

--- myApp.js
+++ myApp.js
@@ -1,14 +1,21 @@
-var express = require('express');
+var express = require("express");
+const dotenv = require("dotenv");
 var app = express();
+
+dotenv.config();
+
+const { MESSAGE_STYLE } = process.env;
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/json", function(req, res) {
-    res.json({"message": "Hello json"});
+app.get("/json", function (req, res) {
+  const message = MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json";
+
+  res.json({ message: message });
 });
 
 module.exports = app;
