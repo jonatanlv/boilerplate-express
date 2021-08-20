fa2ce997f39b900e9c71d942e20bca9bc82b93e9
jonatan.lara <jonatan.lara@gft.com>
2021-08-20 07:51:59 +0200

    Part 7

--- myApp.js
+++ myApp.js
@@ -6,6 +6,11 @@ dotenv.config();
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use(function (req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
